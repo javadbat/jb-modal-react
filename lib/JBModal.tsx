12fb402aac9164caef65a1de7a028d4006914bcb
javadbat <javadbat@gmail.com>
2024-06-27 12:34:41 +0330

    fix: update code for new ts build

--- lib/JBModal.tsx
+++ lib/JBModal.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useImperativeHandle, useRef, useState } from 'react';
 import 'jb-modal';
 // eslint-disable-next-line no-duplicate-imports
 import { JBModalWebComponent } from 'jb-modal';
-import { useEvent } from '../../custom-hooks/UseEvent';
+import { useEvent } from '../../../common/hooks/use-event';
 export type JBModalProps = {
     onClose?: () => void,
     onUrlOpen?: () => void,
@@ -24,63 +24,63 @@ declare global {
     }
 }
 const JBModal = React.forwardRef((props:JBModalProps, ref) => {
-    const element = useRef<JBModalWebComponent>(null);
-    const [refChangeCount, refChangeCountSetter] = useState(0);
-    useImperativeHandle(
-        ref,
-        () => (element ? element.current : {}),
-        [element],
-    );
-    useEffect(() => {
-        refChangeCountSetter(refChangeCount + 1);
-    }, [element.current]);
-    useEffect(() => {
-        if (element.current) {
-            if (props.isOpen == true) {
-                element.current.open();
-            } else {
-                element.current.close();
-            }
-        }
+  const element = useRef<JBModalWebComponent>(null);
+  const [refChangeCount, refChangeCountSetter] = useState(0);
+  useImperativeHandle(
+    ref,
+    () => (element ? element.current : {}),
+    [element],
+  );
+  useEffect(() => {
+    refChangeCountSetter(refChangeCount + 1);
+  }, [element.current]);
+  useEffect(() => {
+    if (element.current) {
+      if (props.isOpen == true) {
+        element.current.open();
+      } else {
+        element.current.close();
+      }
+    }
 
-    }, [props.isOpen]);
-    useEffect(() => {
-        if (props.id !== undefined && element.current) {
-            element.current.addEventListener('urlOpen', onUrlOpen);
-            element.current.id = props.id;
-        }
-        return () => {
-            if (props.id !== undefined && element.current) {
-                element.current.removeEventListener('urlOpen', onUrlOpen);
-            }
-        };
-    }, [props.id]);
-    function onClose() {
-        if (props.onClose) {
-            props.onClose();
-        }
+  }, [props.isOpen]);
+  useEffect(() => {
+    if (props.id !== undefined && element.current) {
+      element.current.addEventListener('urlOpen', onUrlOpen);
+      element.current.id = props.id;
     }
-    function onUrlOpen() {
-        if (props.onUrlOpen) {
-            props.onUrlOpen();
-        }
+    return () => {
+      if (props.id !== undefined && element.current) {
+        element.current.removeEventListener('urlOpen', onUrlOpen);
+      }
+    };
+  }, [props.id]);
+  function onClose() {
+    if (props.onClose) {
+      props.onClose();
     }
-    useEvent(element.current, 'close', onClose);
-    return (
-        <jb-modal ref={element} class={props.className ? props.className : ''}>
-            <div slot="content">
-                {props.children}
-            </div>
-        </jb-modal>
-    );
+  }
+  function onUrlOpen() {
+    if (props.onUrlOpen) {
+      props.onUrlOpen();
+    }
+  }
+  useEvent(element.current, 'close', onClose);
+  return (
+    <jb-modal ref={element} class={props.className ? props.className : ''}>
+      <div slot="content">
+        {props.children}
+      </div>
+    </jb-modal>
+  );
 });
 JBModal.propTypes = {
-    onClose: PropTypes.func,
-    onUrlOpen: PropTypes.func,
-    isOpen: PropTypes.bool.isRequired,
-    children: PropTypes.element,
-    id: PropTypes.string,
-    className:PropTypes.string
+  onClose: PropTypes.func,
+  onUrlOpen: PropTypes.func,
+  isOpen: PropTypes.bool.isRequired,
+  children: PropTypes.element,
+  id: PropTypes.string,
+  className:PropTypes.string
 };
 JBModal.displayName = "JBModal";
 export {JBModal};
